45c74b53d9f5d32a1d475f1df2cdd44453e66fc6
Alp0x <gron141@gmail.com>
2022-03-24 16:08:29 +0100

    Maybe this will work??

--- Program.cs
+++ Program.cs
@@ -9,21 +9,29 @@ builder.Services.AddControllersWithViews();
 builder.Services
     .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options => { options.LoginPath = "/login"; });
-builder.Services.AddSingleton<TableServiceClient>(new TableServiceClient(Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING")));
-builder.Services.AddSingleton<TableClient>(new TableClient(Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING"), "SaltagramTable"));
-var app = builder.Build();
+// builder.Services.AddSingleton<TableServiceClient>(new TableServiceClient(Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING")));
+var storageUri = Environment.GetEnvironmentVariable("AZURE_TABLE_STORAGE_URI");
+var storageAccountKey = Environment.GetEnvironmentVariable("AZURE_TABLE_STORAGE_ACCOUNT_KEY");
+var accountName = Environment.GetEnvironmentVariable("AZURE_TABLE_STORAGE_ACCOUNT_NAME");
+builder.Services.AddSingleton<TableClient>(new TableClient(new Uri(storageUri), "SaltagramTable", new TableSharedKeyCredential(accountName, storageAccountKey)));
 
-// var storageUri = Environment.GetEnvironmentVariable("AZURE_TABLE_STORAGE_URI");
-// var accountName = Environment.GetEnvironmentVariable("AZURE_TABLE_STORAGE_ACCOUNT_NAME"); ;
-// var storageAccountKey = Environment.GetEnvironmentVariable("AZURE_TABLE_STORAGE_ACCOUNT_KEY");
-
+// var connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
 // var serviceClient = new TableServiceClient(
 //     new Uri(storageUri),
 //     new TableSharedKeyCredential(accountName, storageAccountKey));
-// string tableName = "CreatedWithCodeTable";
+// string tableName = "SaltagramTable";
 
 // TableItem table = serviceClient.CreateTableIfNotExists(tableName);
+
+// var tableClient = new TableClient(
+// new Uri(storageUri),
+// "SaltagramTable",
+// new TableSharedKeyCredential(accountName, storageAccountKey));
+
+// tableClient.Create();
+
 // Configure the HTTP request pipeline.
+var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
